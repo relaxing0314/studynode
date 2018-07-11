981092ef7d2fe418100fa3032bf0172a092a9d15
娜娜 <relaxing0314@163.com>
2018-07-11 17:33:25 +0800

    关于我

---
+++ flex/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container id="app">
+    <el-header>
+      <Header></Header>
+    </el-header>
+    <el-main class="content">Main</el-main>
+    <el-footer class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
